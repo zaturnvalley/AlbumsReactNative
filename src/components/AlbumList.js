593f3c285833f3dc9313e552c61d8f913485dded
zaturnvalley <zacharycmason@gmail.com>
2017-12-07 21:33:43 -0800

    created album list component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumList = () => {
+  return (
+    <View>
+      <Text>Album list</Text>
+    </View>
+  );
+};
+
+export default AlbumList;
