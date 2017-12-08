548c6ddb5c0265343f5a2befb8948d98c20f3d84
zaturnvalley <zacharycmason@gmail.com>
2017-12-07 21:50:29 -0800

    refactored as classbased component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album list</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  render() {
+      return (
+      <View>
+        <Text>Album list</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
