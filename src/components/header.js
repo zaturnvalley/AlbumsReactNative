e0d82b0119493ae6cce2e43e97021380ffb7f002
zaturnvalley <zacharycmason@gmail.com>
2017-12-07 21:05:08 -0800

    added styling to header view

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,34 @@
 // Import lib for making component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make component
 const Header = () => {
-  return <Text>Albums</Text>;
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>
+    </View>
+    );
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    height: 60,
+    paddingTop: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.4,
+    elevation: 2,
+    position: 'relative'
+  }
 };
 
 // Make component available to other parts of app
