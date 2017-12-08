ea66e012f5467d926bbf69ea57301289d8c77f23
zaturnvalley <zacharycmason@gmail.com>
2017-12-07 22:23:19 -0800

    added api call

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
   componentWillMount() {
-    console.log('hi');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response));
   }
   render() {
       return (
