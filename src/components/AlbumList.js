bea12e784a51a1e32f0b29ad6e9be38a9a7a04fe
zaturnvalley <zacharycmason@gmail.com>
2017-12-07 21:59:39 -0800

    lifecycle method implemented, ex

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+  componentWillMount() {
+    console.log('hi');
+  }
   render() {
       return (
       <View>
