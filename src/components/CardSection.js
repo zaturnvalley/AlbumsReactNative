8910ca07bce4a12f0a398efb8a294937859b9ef8
zaturnvalley <zacharycmason@gmail.com>
2017-12-08 14:08:01 -0800

    added card section component

---
+++ src/components/CardSection.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CardSection = () => {
+  return (
+    <View></View>
+  );
+};
+
+const style = {
+  containerStyle: {
+    borderBottomWidth: 1
+
+  }
+};
+
+export default CardSection;
