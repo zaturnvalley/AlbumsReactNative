9b6760883902a2f8ccce0f3734a2fc917b5f84e6
zaturnvalley <zacharycmason@gmail.com>
2017-12-08 09:24:35 -0800

    added renderAlbums method

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,14 +3,21 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
+      .then(response => this.setState({ albums: response.data }));
   }
+
+  renderAlbums() {
+    return this.state.albums.map((album) => <Text>{album.title}</Text>);
+  }
+
   render() {
       return (
       <View>
-        <Text>Album list</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
