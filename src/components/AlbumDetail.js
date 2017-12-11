8e52d95f6dab96bfd27043957ab240ad15933a4c
zaturnvalley <zacharycmason@gmail.com>
2017-12-10 23:25:18 -0800

    pulls in artist and title

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -7,7 +7,11 @@ const AlbumDetail = (props) => {
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View></View>
+        <View>
+          <Text>{props.album.title}</Text>
+          <Text>{props.album.artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
