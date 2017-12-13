4ab8aa073c7fdd8bc7e59aab4024f9760d7858e5
zaturnvalley <zacharycmason@gmail.com>
2017-12-13 15:39:25 -0800

    added button style

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
+  const { buttonStyle } = styles;
   return (
-    <TouchableOpacity>
+    <TouchableOpacity style={buttonStyle} >
       <Text>Purchase</Text>
     </TouchableOpacity>
   );
+};
+
+const styles = {
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  }
 };
 
 export default Button;
