472424c01c217063ddd9429eb8494e54ab24627a
zaturnvalley <zacharycmason@gmail.com>
2017-12-13 12:34:37 -0800

    added album image section and style

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,12 +4,13 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, image } = album;
+  const { title, artist, thumbnail_image, image } = album;
   const { 
     thumbnailStyle, 
     headerContentStyle,
     thumbnailContainerStyle,
-    headerTextStyle
+    headerTextStyle,
+    imageStyle
   } = styles;
   return (
     <Card>
@@ -17,13 +18,19 @@ const AlbumDetail = ({ album }) => {
         <View style={thumbnailContainerStyle}>
           <Image 
           style={thumbnailStyle} 
-          source={{ uri: image }} 
+          source={{ uri: thumbnail_image }} 
           />
         </View>
         <View style={headerContentStyle}>
           <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image 
+          style={imageStyle}
+          source={{ uri: image }} 
+          />
       </CardSection>
     </Card>
   );
@@ -46,6 +53,11 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
