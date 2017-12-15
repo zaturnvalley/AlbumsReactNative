a3ce9b0868c38b00132c780e7b350d1fadb13a1e
zaturnvalley <zacharycmason@gmail.com>
2017-12-14 21:35:26 -0800

    passed function to component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = () => {
+const Button = ({ onPress }) => {
   const { buttonStyle, textStyle } = styles;
   return (
-    <TouchableOpacity onPress={() => console.log('pressed')} style={buttonStyle} >
+    <TouchableOpacity onPress={onPress} style={buttonStyle} >
       <Text style={textStyle} >
         Purchase
       </Text>
